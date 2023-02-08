70c90bdbc1812afdbf76e4fca63bcb0456b516b4
Roberta Aguilar <roberta_rfa@hotmail.com>
2023-02-08 09:07:46 -0300

    fixColab

--- Projeto-OpenCv/faceDetection.py
+++ Projeto-OpenCv/faceDetection.py
@@ -1,9 +1,13 @@
+from google.colab.patches import cv2_imshow
 import numpy as np
 import cv2 as cv
+# from google.colab.patches import cv2_imshow Para usar no Colab
 
 face_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 image = cv.imread('https://media.istockphoto.com/id/1346125184/photo/group-of-successful-multiethnic-business-team.jpg?s=612x612&w=0&k=20&c=5FHgRQZSZed536rHji6w8o5Hco9JVMRe8bpgTa69hE8=')
+
+# image = cv.imread('image.jpg', cv.IMREAD_UNCHANGED) Para usar no Colab
 image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 faces = face_classifier.detectMutiScale(image_gray, 1.3, 5)
@@ -11,6 +15,7 @@ faces = face_classifier.detectMutiScale(image_gray, 1.3, 5)
 for(x, y, w, h) in faces:
     cv.rectangle(image, (x, y), (x+w+h), (255, 0, 0), 2)
 
+# cv2_imshow(image) Para usar no Colab
 cv.imshow('imagem', image)
 cv.waitKey(0)
 cv.destroyAllWindows()
